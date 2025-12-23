a7d719edc1d566e0db9280506740a753f135cb36
Tausif Rahman <69134814+tawsif0@users.noreply.github.com>
2025-12-23 12:51:11 +0600

    Update User.js.

--- backend/models/User.js
+++ backend/models/User.js
@@ -42,7 +42,7 @@ const userSchema = new mongoose.Schema({
   status: {
     type: String,
     enum: ["active", "inactive"],
-    default: "active",
+    default: "inactive",
   },
 
   passwordResetToken: String,
